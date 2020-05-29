726d4955a599c123e554eadc7bab7791695a7215
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2020-05-29 17:31:34 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/model/TitleDataObject.php
+++ code/model/TitleDataObject.php
@@ -15,6 +15,20 @@ class TitleDataObject extends DataObject
         'Title' => "ASC"
     );
 
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'TitleDataObject';
+
     private static $db = array(
         'Title' => 'Varchar(255)',
     );
